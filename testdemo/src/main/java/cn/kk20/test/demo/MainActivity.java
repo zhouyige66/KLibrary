e8f79b0838f4364e2800fb822146b96b32b851b8
kk20 <751664206@qq.com>
2017-06-22 11:44:47 +0800

    测试activit生命周期

--- testdemo/src/main/java/cn/kk20/test/demo/MainActivity.java
+++ testdemo/src/main/java/cn/kk20/test/demo/MainActivity.java
@@ -1,6 +1,10 @@
 package cn.kk20.test.demo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.AttributeSet;
 import android.view.View;
 
 import org.xutils.view.annotation.ContentView;
@@ -12,15 +16,83 @@ import cn.kk20.test.demo.base.IBaseActivity;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends IBaseActivity {
 
-    @Event(R.id.button)
-    private void testCrash(View view) {
-        ILogUtils.i("点击按钮");
-        String str = "test";
-        System.out.print(str.substring(0, 6));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ILogUtils.i("调用了onCreate()");
+
+        if(savedInstanceState!=null){
+            Bundle mBundle = savedInstanceState.getBundle("custom");
+            if (mBundle != null) {
+                ILogUtils.i("保存数据为：" + mBundle.getString("data"));
+            }
+        }
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        ILogUtils.i("调用了onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ILogUtils.i("调用了onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ILogUtils.i("调用了onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ILogUtils.i("调用了onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ILogUtils.i("调用了onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ILogUtils.i("调用了onDestroy()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Bundle mBundle = new Bundle();
+        mBundle.putString("data", "kk20");
+        outState.putBundle("custom", mBundle);
+        super.onSaveInstanceState(outState);
+        ILogUtils.i("调用了onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ILogUtils.i("调用了onRestoreInstanceState()");
+
+        Bundle mBundle = savedInstanceState.getBundle("custom");
+        ILogUtils.i("保存数据为：" + mBundle.getString("data"));
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        ILogUtils.i("调用了onCreateView()");
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Event(R.id.button)
+    private void testCrash(View view) {
+        startActivity(new Intent(this, TestActivity.class));
+    }
+
 }
+
